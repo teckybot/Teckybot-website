8808f2213bb0d44667f981a1ee30f65f7aa14378
SAPPA SAI <sappasai.msccs@gmail.com>
2025-06-25 18:19:54 +0530

    new updated project

--- src/Components/ImageCarousel2.jsx
+++ src/Components/ImageCarousel2.jsx
@@ -4,10 +4,9 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-
 const ImageCarousel1 = ({ images }) => {
   return (
-    <div className="w-[1500px] " >
+    <div className="w-full max-w-7xl mx-auto">
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         spaceBetween={0}
@@ -20,11 +19,13 @@ const ImageCarousel1 = ({ images }) => {
       >
         {images.map((img, index) => (
           <SwiperSlide key={index}>
-            <img
-              src={img.src}
-              alt={img.alt || `Slide ${index + 1}`}
-              className="w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] object-cover rounded-3xl"
-            />
+            <div className="w-full aspect-[16/9] sm:aspect-[21/9] md:aspect-[16/7] lg:aspect-[16/6] xl:aspect-[16/5] flex items-center justify-center">
+              <img
+                src={img.src}
+                alt={img.alt || `Slide ${index + 1}`}
+                className="w-full h-full object-cover rounded-3xl"
+              />
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
@@ -33,4 +34,3 @@ const ImageCarousel1 = ({ images }) => {
 };
 
 export default ImageCarousel1;
-
