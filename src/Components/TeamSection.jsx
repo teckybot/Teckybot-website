0ed02d949da422e0df67697fcbd3fd6309a38bf7
B.K.S.S.S MALLIK <sssmallik143@gmail.com>
2025-10-17 12:23:59 +0530

    new members added

--- src/Components/TeamSection.jsx
+++ src/Components/TeamSection.jsx
@@ -4,12 +4,12 @@ import img2 from '../data/aboutus/team/HR Manager_teckybot.jpg'
 import img3 from '../data/aboutus/team/HR Associate_teckybot.jpg'
 import img4 from '../data/aboutus/team/Senior Developer (ES)_teckybot.jpg'
 import img5 from '../data/aboutus/team/Senior Developer (Robotics)_teckybot.jpg'
-import img6 from '../data/aboutus/team/Senior Developer (3D Printing)_teckybot.jpg'
+import img6 from '../data/aboutus/team/Business Operations Coordinator_teckybot.JPG'
 import img7 from '../data/aboutus/team/Senior Developer (AI)_teckybot.jpg'
 import img8 from '../data/aboutus/team/Business Dev. Manager__teckybot.jpg'
 import img9 from '../data/aboutus/team/Digital Marketing Executive_teckybot.jpg'
 import img10 from '../data/aboutus/team/Business Development manager_teckybot.jpg'
-import img11 from '../data/aboutus/team/Technical Trainer_teckybot.jpg'
+import img11 from '../data/aboutus/team/Full_Stack_Developer_teckybot.jpg'
 import img12 from '../data/aboutus/team/Technical Trainer2_teckybot.jpg'
 import img13 from '../data/aboutus/team/Technical Trainer1_teckybot.png'
 import img14 from '../data/aboutus/team/Full Stack Developer_teckybot.jpg'
@@ -47,16 +47,16 @@ const teamMembers = [
         linkedInUrl: 'https://www.linkedin.com/in/vijay-patro-38484112b/',
     },
     {
-        name: 'A. RAJESH',
-        position: 'Senior Developer (3D Printing)',
-        imageUrl: img6,
-        linkedInUrl: 'https://www.linkedin.com/in/arjilli-rajesh-1632781b2/',
-    },
-    {
         name: 'B. BHARGAVI',
         position: 'Senior Developer (AI)',
         imageUrl: img7,
         linkedInUrl: 'https://www.linkedin.com/in/bandaru-bhargavi-3b1340217/',
+    },
+    {
+        name: 'CH. JOSHINI',
+        position: 'Business Operations Coordinator',
+        imageUrl: img6,
+        linkedInUrl: 'https://www.linkedin.com/in/joshinichataparthi/',
     },
     {
         name: 'B. DURGA PRASAD ',
@@ -65,22 +65,16 @@ const teamMembers = [
         linkedInUrl: 'https://www.linkedin.com/in/durgaprasad-bonagiri-684408172/',
     },
     {
-        name: 'S. DEVI KIRAN',
-        position: 'Digital Marketing Executive',
-        imageUrl: img9,
-        linkedInUrl: 'https://www.linkedin.com/in/devikiransanapala/',
-    },
-    {
         name: 'T. KARTHEEK',
         position: 'Business Development Manager',
         imageUrl: img10,
         linkedInUrl: 'https://www.linkedin.com/in/kartheek-thalluru-885b90179/',
     },
     {
-        name: 'P. NARESH',
-        position: 'Technical Trainer',
-        imageUrl: img11,
-        linkedInUrl: 'https://www.linkedin.com/in/p-naresh-09a5612a9/',
+        name: 'S. DEVI KIRAN',
+        position: 'Digital Marketing Executive',
+        imageUrl: img9,
+        linkedInUrl: 'https://www.linkedin.com/in/devikiransanapala/',
     },
     {
         name: 'C. Sai Charan',
@@ -100,7 +94,12 @@ const teamMembers = [
         imageUrl: img14,
         linkedInUrl: 'https://www.linkedin.com/in/bksssmallik/',
     },
-    
+    {
+        name: 'SAPPA SAI',
+        position: 'Full Stack Developer',
+        imageUrl: img11,
+        linkedInUrl: 'https://www.linkedin.com/in/sappasai/',
+    },
     {
         name: 'N. DINESH',
         position: 'Supply Chain Manager',
